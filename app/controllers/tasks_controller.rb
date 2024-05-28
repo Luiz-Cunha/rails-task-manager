21e48c4d748d7026856df8fe77fff1672523d4cf
Luiz Eduardo <cunhaworkspace@gmail.com>
2024-05-28 15:32:44 -0400

    All Tasks MVC using Rails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+
   end
 
   def new
@@ -19,24 +20,25 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(strong_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    # No need for app/views/restaurants/destroy.html.erb
     redirect_to tasks_path, status: :see_other
   end
 
   private
   def strong_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,19 +7,7 @@ Rails.application.routes.draw do
 
   # Routes
 
-  get "tasks/new", to: "tasks#new", as: :new_task
-
-  get "tasks", to: "tasks#index", as: :tasks
-
-  post "tasks", to: "tasks#create"
-
-  delete "tasks/:id", to: "tasks#destroy"
-
-  get "tasks/:id", to: "tasks#show", as: :task
-
-  patch "tasks/:id", to: "tasks#update"
-
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  resources :tasks
 
   # Defines the root path route ("/")
   # root "posts#index"
